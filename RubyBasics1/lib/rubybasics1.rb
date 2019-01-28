54e653e839e8677012699ca416034d0c36b15127
EC2 Default User <ec2-user@ip-172-31-15-129.us-east-2.compute.internal>
2019-01-28 05:47:38 +0000

    v3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -1,26 +1,3 @@
-# Lab 1
-# Part I
-def sum arr
-  total = 0
-  arr.each do |x|
-    total += x
-  end
-  return total
-end
-
-# Part II
-def max_2_sum arr
-  if (arr.length == 0)
-    return 0
-  end
-  if (arr.length == 1)
-    return arr[0]
-  end
-  sorted_arr = arr.sort
-  return sorted_arr[-1]+sorted_arr[-2]
-end
-
-# Part III
 def sum_to_n? arr, n
   if (arr.length == 0)
     return false
