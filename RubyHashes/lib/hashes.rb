88622c1ce3371f9da70d1c970f81a0596bfd2d3f
EC2 Default User <ec2-user@ip-172-31-15-129.us-east-2.compute.internal>
2019-03-09 08:38:08 +0000

    commitign everything

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -1,45 +1,26 @@
-# RubyHashes
-# Part I
-def array_2_hash emails, contacts
-    count = 0
-    if !emails.empty?
-        contacts.each do |key, value|
-           contacts[key] = emails[count]
-           count +=1
-        end
+def squares(a,b,c)
+   return {a=>a*a,b=>b*b,c=>c*c} 
+end
+def add(*a)
+    result =0
+    a.each do |x|
+        result +=x
     end
-    return contacts
+    return result
 end
 
-
-# Part II
-def array2d_2_hash contact_info, contacts
-    count = 0
-    if !contact_info.empty? && !contact_info[0].empty?
-        contacts.each do |key, value|
-            contacts[key] = {email: contact_info[count][0], phone: contact_info[count][1]}
-            count+=1
-        end
-    end
-    return contacts
+def prints result
+    print "tj #{result}"
 end
 
-
-# Part III
-def hash_2_array contacts
-    array1 = Array.new(3) { Array.new }
-    count = 0;
-    contacts.each do |key, value|
-        value.each do |key1, value1|
-            array1[count].push(value1)
-            count += 1
-        end
-        array1[count].push("#{key}")
-        count = 0;
-    end
-    return array1
-    
+def foo(arg,hash1,hash2)
+      
 end
 
-
-
+h = {1=>2, 2=>8, 3=>73}
+sum = 0
+h.each do |k, v|
+  sum+=k
+  puts k
+end
+puts sum
